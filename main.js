d643ff698c515fd67310daac55b89ceef5815c70
NeoSmait <neosmait@ya.ru>
2021-11-02 22:04:25 +0900

    clock

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-// заменим цвет фона на красный,
 document.body.style.background = "red";
 
-// а через секунду вернём как было
 setTimeout(() => document.body.style.background = "", 1000);
+
+let clock = document.getElementById("clock");
+ 
+// Запускаем интервал
+setInterval(function () {
+    // Создаём объект класса Date
+    let date = new Date()
+    // Получаем часы
+    let hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
+    // Получаем минуты
+    let minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+    // Получаем секунды
+    let seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    // Создаём строку для вывода времени
+    strClocke = `${hours}:${minutes}<small>:${seconds}</small>`;
+    // Выводим время
+    clock.innerHTML = strClocke;
+}, 1000);
