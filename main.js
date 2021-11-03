74b5936ffa159eccd291371ec3955bcb7f54ace4
NeoSmait <neosmait@ya.ru>
2021-11-03 21:36:56 +0900

    master

--- main.js
+++ main.js
@@ -1,36 +1,53 @@
-document.body.style.background = "red";
-
+document.body.style.background = "#A62103";
 setTimeout(() => document.body.style.background = "", 1000);
 
 let clock = document.getElementById("clock");
 let appDays = document.getElementById("ndays");
+let appMonth = document.getElementById("nmonths");
 
-// Запускаем интервал
+let date = new Date();
+
+let nowdays = date.getDay();
+let nowyears = date.getFullYear();
+
+const months = [
+	'января',
+	'февраля',
+	'марта',
+	'апреля',
+	'мая',
+	'июня',
+	'июля',
+	'августа',
+	'сентября',
+	'октября',
+	'ноября',
+	'декабря'
+];
+let m = date.getMonth();
+strMonths = `${nowdays} ${months[m]} ${nowyears} года`;
+appMonth.innerHTML = strMonths;
+
+const days = [
+	'воскресенье',
+	'понедельник',
+	'вторник',
+	'среда',
+	'четверг',
+	'пятница',
+	'суббота'
+];
+let n = date.getDay();
+strDays = `Сегодня ${days[n]}`;
+appDays.innerHTML = strDays;
+
+
 setInterval(function () {
-	// Создаём объект класса Date
-	let date = new Date()
-	// Создаем масив с днями недели
-	let days = [
-		'воскресенье',
-		'понедельник',
-		'вторник',
-		'среда',
-		'четверг',
-		'пятница',
-		'суббота'
-	];
-	let n = date.getDay();
-	// Создаём строку для вывода дня недели
-	strDays = `Сегодня ${days[n]}`;
-	appDays.innerHTML = strDays;
-	// Получаем часы
+	let date = new Date();
 	let hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
-	// Получаем минуты
 	let minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
-	// Получаем секунды
 	let seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
-	// Создаём строку для вывода времени
+
 	strClocke = `${hours}:${minutes}<small>:${seconds}</small>`;
-	// Выводим время
 	clock.innerHTML = strClocke;
 }, 1000);
